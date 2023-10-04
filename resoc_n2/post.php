b896fad85c8321c3d65c7d75ca43225197fbdc3a
Jay <gilbertmougamadou@yahoo.fr>
2023-10-04 13:12:51 +0200

    gestion des likes

--- resoc_n2/post.php
+++ resoc_n2/post.php
@@ -1,3 +1,31 @@
+<?php
+
+$traitementLike = isset($_POST['like'], $_POST['postIdtoLike']);
+    if ($traitementLike) {
+        $sessionId = $_SESSION['connected_id'];
+        $postId = $_POST['postIdtoLike'];
+        $sessionId = intval($mysqli->real_escape_string($sessionId));
+        $postId = intval($mysqli->real_escape_string($postId));
+
+        $sql = "SELECT * FROM likes WHERE user_id = $sessionId AND post_id = $postId";
+        $result = $mysqli->query($sql);
+
+        if ($result->num_rows == 0) {
+
+        $lInstructionSql = "INSERT INTO likes "
+            . "(id, user_id, post_id) "
+            . "VALUES (NULL, "
+            . $sessionId . ", "
+            . $postId . ");";
+    echo $lInstructionSql;
+
+    $ok = $mysqli->query($lInstructionSql);
+    if (!$ok) {
+        echo "Impossible de liker ce post" . $mysqli->error;
+    }
+    }
+}
+?>
 <article>
     <h3>
         <time><?php echo $post['created'] ?></time>
@@ -11,6 +39,11 @@
     <footer>
         <small>
             ♥ <?php echo $post['like_number'] ?>
+            <form action="<?php echo $_SERVER['PHP_SELF'] . "?" . $_SERVER['QUERY_STRING'] ?>" method="post">
+                        <input type='hidden' name='like' value='true'>
+                        <input type='hidden' name='postIdtoLike' value=<?php echo $post['post_id'] ?>>
+                        <input type='submit' value="Like">
+                    </form>
         </small>
         <?php
         $tagsArray = explode(',', $post['taglist']);
