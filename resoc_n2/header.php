8a4e1024dbfa8041fc195e4048a7d942c78a6551
Jezzatator <jeremie.patot@protonmail.ch>
2023-10-05 10:01:34 +0200

    finalisation de l'objectif 2

--- resoc_n2/header.php
+++ resoc_n2/header.php
@@ -9,12 +9,23 @@ session_start();
     <a href="feed.php?user_id=<?php echo $_SESSION['connected_id'] ?>">Flux</a>
     <a href="tags.php?tag_id=1">Mots-clés</a>
 </nav>
+
+
 <nav id="user">
-    <a href="#">Profil</a>
-    <ul>
-        <li><a href="settings.php?user_id=<?php echo $_SESSION['connected_id'] ?>">Paramètres</a></li>
-        <li><a href="followers.php?user_id=<?php echo $_SESSION['connected_id'] ?>">Mes suiveurs</a></li>
-        <li><a href="subscriptions.php?user_id=<?php echo $_SESSION['connected_id'] ?>">Mes abonnements</a></li>
-    </ul>
+
+    <?php if ($_SESSION['connected_id']) { ?>
+
+        <a href="#">Profil</a>
+        <ul>
+            <li><a href="settings.php?user_id=<?php echo $_SESSION['connected_id'] ?>">Paramètres</a></li>
+            <li><a href="followers.php?user_id=<?php echo $_SESSION['connected_id'] ?>">Mes suiveurs</a></li>
+            <li><a href="subscriptions.php?user_id=<?php echo $_SESSION['connected_id'] ?>">Mes abonnements</a></li>
+        </ul>
+
+    <?php
+    } else {
+    ?>
+        <a href="login.php">Connexion</a>
+    <?php } ?>
 
 </nav>
