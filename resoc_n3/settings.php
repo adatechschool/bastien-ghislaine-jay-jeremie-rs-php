155a8779163e1cd6d1ffb3596d4f50c80170868a
Jay <gilbertmougamadou@yahoo.fr>
2023-10-10 12:54:51 +0200

    edit settings.php with all the user's profile pictures and update the socialnetwork.sql

--- resoc_n3/settings.php
+++ resoc_n3/settings.php
@@ -21,9 +21,37 @@ session_start();
     </header>
     <div id="wrapper" class='profile'>
 
+    <?php 
+    require_once 'logingSQL.php'; 
+    // Vérifier si user_id est défini dans l'URL
+    if (isset($_GET['user_id'])) {
+        $userId = intval($_GET['user_id']);
+
+        // Récupérer les données de l'image depuis la base de données
+        $result = $mysqli->query("SELECT type, bin FROM images WHERE user_id = $userId ORDER BY id DESC LIMIT 1");
+
+        if ($result !== false && $result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            $imageData = base64_encode($row['bin']);
+            $imageSrc = "data:" . $row['type'] . ";base64," . $imageData;
+        } else {
+            // Si l'utilisateur n'a pas de photo de profil personnalisée, utilisez une image par défaut
+            $imageSrc = "chemin/vers/dossier/images/default.jpg";
+        }
+        } else {
+        // Si user_id n'est pas défini dans l'URL, affichez un message d'erreur
+        echo "L'identifiant de l'utilisateur n'est pas défini.";
+    }
+    ?>
 
         <aside>
-            <img src="user.jpg" alt="Portrait de l'utilisatrice" />
+            <?php if(isset($imageSrc)){ ?> 
+                <div class="gallery"> 
+                    <img src="<?php echo $imageSrc; ?>" alt="Portrait de l'utilisateur" />
+                </div> 
+            <?php }else{ ?> 
+                 <p class="status error">Image not found...</p> 
+            <?php } ?>
             <section>
                 <h3>Présentation des paramètres</h3>
                 <p>Sur cette page vous trouverez l'ensemble de vos informations.</p>
@@ -125,7 +153,7 @@ session_start();
                             <form action="" method="post" enctype="multipart/form-data">
                                 <input type="hidden" name="max_file-size" value="250000" />
                                 <input type="file" name="image" /><br />
-                                <input type="submit" name="valider" value="charger" />
+                                <input type="submit" name="valider" value="Sauvegarder" />
                             </form>
                 </article>
             <?php
